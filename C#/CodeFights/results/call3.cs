90d856bf3cf382de71034367cc4d69ccffc10800
Jakub Klein <jakub.klein@visma.com>
2023-02-23 17:47:32 +0100

    second

--- C#/CodeFights/results/call3.cs
+++ C#/CodeFights/results/call3.cs
@@ -1,43 +1,106 @@
-public static int ConvertFromRoman(string romanNumeral)
+public static class RomanNumeralConverter
 {
-    if (string.IsNullOrEmpty(romanNumeral)) // handle empty input
-        throw new ArgumentException("Input cannot be null or empty.", nameof(romanNumeral));
-
-    var romanDict = new Dictionary<string, int>()
+    private static readonly string[] RomanNumerals =
     {
-        { "M", 1000 },
-        { "CM", 900 },
-        { "D", 500 },
-        { "CD", 400 },
-        { "C", 100 },
-        { "XC", 90 },
-        { "L", 50 },
-        { "XL", 40 },
-        { "X", 10 },
-        { "IX", 9 },
-        { "V", 5 },
-        { "IV", 4 },
-        { "I", 1 }
+        "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
     };
 
-    var result = 0;
-    var i = 0;
-    while (i < romanNumeral.Length)
+    private static readonly int[] DecimalValues =
     {
-        if (i + 1 < romanNumeral.Length && romanDict.TryGetValue(romanNumeral.Substring(i, 2), out var value))
+        1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+    };
+
+    public static string ConvertToRoman(int number)
+    {
+        if (number < 1 || number > 3999)
         {
-            result += value;
-            i += 2;
+            throw new ArgumentOutOfRangeException(nameof(number), "Roman numerals can only represent integers between 1 and 3999.");
         }
-        else if (romanDict.TryGetValue(romanNumeral.Substring(i, 1), out var value))
+
+        var result = new StringBuilder();
+
+        for (var i = 0; i < RomanNumerals.Length; i++)
         {
-            result += value;
-            i += 1;
+            while (number >= DecimalValues[i])
+            {
+                result.Append(RomanNumerals[i]);
+                number -= DecimalValues[i];
+            }
         }
-        else
-        {
-            throw new ArgumentException("Input is not a valid Roman numeral.", nameof(romanNumeral));
-        }
+
+        return result.ToString();
     }
-    return result;
+
+    public static int ConvertFromRoman(string romanNumeral)
+    {
+        if (string.IsNullOrEmpty(romanNumeral))
+        {
+            throw new ArgumentException("Input string cannot be null or empty.", nameof(romanNumeral));
+        }
+
+        romanNumeral = romanNumeral.ToUpperInvariant();
+
+        var result = 0;
+        var i = 0;
+
+        while (i < romanNumeral.Length)
+        {
+            var currentSymbol = romanNumeral[i];
+
+            if (!IsRomanNumeral(currentSymbol))
+            {
+                throw new ArgumentException($"Invalid character '{currentSymbol}' in input string.", nameof(romanNumeral));
+            }
+
+            var currentDecimalValue = GetDecimalValue(currentSymbol);
+
+            if (i == romanNumeral.Length - 1 || currentDecimalValue >= GetDecimalValue(romanNumeral[i + 1]))
+            {
+                result += currentDecimalValue;
+                i++;
+            }
+            else
+            {
+                result += GetDecimalValue(romanNumeral[i + 1]) - currentDecimalValue;
+                i += 2;
+            }
+        }
+
+        if (result < 1 || result > 3999)
+        {
+            throw new ArgumentOutOfRangeException(nameof(romanNumeral), "Roman numerals can only represent integers between 1 and 3999.");
+        }
+
+        return result;
+    }
+
+    private static bool IsRomanNumeral(char symbol)
+    {
+        return symbol switch
+        {
+            'I' => true,
+            'V' => true,
+            'X' => true,
+            'L' => true,
+            'C' => true,
+            'D' => true,
+            'M' => true,
+            _ => false
+        };
+    }
+
+    private static int GetDecimalValue(char symbol)
+    {
+        return symbol switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => throw new ArgumentException($"Invalid character '{symbol}' in input string.")
+        };
+    }
 }
