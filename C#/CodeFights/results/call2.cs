f92179ee427fb58ad6d2ab6b3d5437dbf2e45694
Jakub Klein <jakub.klein@visma.com>
2023-02-23 17:54:02 +0100

    second

--- C#/CodeFights/results/call2.cs
+++ C#/CodeFights/results/call2.cs
@@ -1,35 +1,47 @@
-public static string[] DirectionsSolution(string[] arr)
-{
-    Stack<string> stack = new Stack<string>();
-    foreach (string direction in arr)
-    {
-        if (stack.Count > 0 && stack.Peek() == OppositeDirection(direction))
-        {
-            stack.Pop();
-        }
-        else
-        {
-            stack.Push(direction);
-        }
-    }
-    string[] result = stack.ToArray();
-    Array.Reverse(result);
-    return result;
-}
+using System;
 
-private static string OppositeDirection(string direction)
+public class Program
 {
-    switch (direction)
+    public static void Main()
     {
-        case "NORTH":
-            return "SOUTH";
-        case "SOUTH":
-            return "NORTH";
-        case "EAST":
-            return "WEST";
-        case "WEST":
-            return "EAST";
-        default:
-            throw new ArgumentException("Invalid direction: " + direction);
+        string[] arr = { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+        string[] result = DirectionsSolution(arr);
+        Console.WriteLine(string.Join(", ", result));
+    }
+
+    public static string[] DirectionsSolution(string[] arr)
+    {
+        Stack<string> stack = new Stack<string>();
+        foreach (string direction in arr)
+        {
+            if (stack.Count > 0 && stack.Peek() == OppositeDirection(direction))
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(direction);
+            }
+        }
+        string[] result = stack.ToArray();
+        Array.Reverse(result);
+        return result;
+    }
+
+    private static string OppositeDirection(string direction)
+    {
+        switch (direction)
+        {
+            case "NORTH":
+                return "SOUTH";
+            case "SOUTH":
+                return "NORTH";
+            case "EAST":
+                return "WEST";
+            case "WEST":
+                return "EAST";
+            default:
+                throw new ArgumentException("Invalid direction: " + direction);
+        }
     }
 }
